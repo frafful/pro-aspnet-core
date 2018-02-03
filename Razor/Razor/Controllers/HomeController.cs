9dd9a14097ab769dacbcc7be247a5e80c322eee4
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-02-03 14:06:32 -0200

    First Commit

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275m
+            };
+
+            ViewBag.StockLevel = 3;
+
+            return View(myProduct);
+        }
+    }
+}
---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
+        {
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert 
+            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
+        {
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns( new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object };
+
+            // Act 
+            var result = controller.Index();
+
+            // Assert 
+            mock.VerifyGet(m => m.Products, Times.Once);
+        }
+    }
+}
