c03042219e8fd9ad1e1ddc6a473838296166f632
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-02-13 22:52:18 -0200

    Chapter 14 pt. 1

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(new Dictionary<string, string> { ["Message"] = "This is the Index Action." });
+    }
+}
