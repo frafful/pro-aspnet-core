a98eaca3ef45f409af5ecf60c2046146fa479c8e
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-02-18 22:53:37 -0300

    Chapter 14 pt.2

--- ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View(new Dictionary<string, string> { ["Message"] = "This is the Index Action." });
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up) => uptime = up;
+
+        public IActionResult Index(bool throwException = false)
+        {
+            if (throwException)
+            {
+                throw new System.NullReferenceException();
+            }
+            return View(new Dictionary<string, string> { ["Message"] = "This is the Index Action.", ["Uptime"] = $"{uptime.Uptime}ms" });
+        }
+
+        public ViewResult Error() => View(nameof(Index), new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Error action"
+        });
     }
 }
--- ConfiguringApps/ConfiguringApps/Startup.cs
+++ ConfiguringApps/ConfiguringApps/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ConfiguringApps.Infrastructure;
 
 namespace ConfiguringApps
 {
@@ -15,6 +16,8 @@ namespace ConfiguringApps
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +26,21 @@ namespace ConfiguringApps
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error/");
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
